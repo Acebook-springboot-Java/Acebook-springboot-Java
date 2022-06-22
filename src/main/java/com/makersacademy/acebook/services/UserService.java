7a3c874fe9798c9728c918dedfc4efeb22a12fa5
Matt Clements <matthewlclements@gmail.com>
2022-06-22 11:17:52 +0100

    Edited controller, changed schema & tested login

--- src/main/java/com/makersacademy/acebook/services/UserService.java
+++ src/main/java/com/makersacademy/acebook/services/UserService.java
@@ -22,24 +22,25 @@ public class UserService {
   private SecurityConfiguration securityConfiguration;
 
   public User createUser(User user){
+      System.out.println(user);
       PasswordEncoder encoder = securityConfiguration.passwordEncoder();
       //user.setUsername(user.getUsername());
       String password = user.getPassword();
-      /* 
+      
       System.out.println("***userpass****");
       System.out.println(password);
       System.out.println("***userpass****");
-      */
+    
       String encodedPassword = encoder.encode(password);
-      /*
+      
       System.out.println("***encodedPass****");
       System.out.println(encodedPassword);
       System.out.println("***encodedPass****");
-      */
+    
       user.setPassword(encodedPassword);
       Authority authority = new Authority(user.getUsername(), "ROLE_USER");
-      authoritiesRepository.save(authority);
       userRepository.save(user);
+      authoritiesRepository.save(authority);
       return user;
   }
 }
