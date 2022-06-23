7272d3d18d86aedf876ed5fb9be24e2da8dcfecf
winstoncwang <congwang@live.co.uk>
2022-06-23 17:05:07 +0100

    sercurityConfig

--- src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
@@ -9,59 +9,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.makersacademy.acebook.security.*;
+import com.makersacademy.acebook.security.JWTAuthenticationFilter;
+import com.makersacademy.acebook.security.JWTAuthorizationFilter;
+import com.makersacademy.acebook.services.UserService;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private UserDetailsService userDetailsService;
+    private UserService userService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public SecurityConfiguration(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-        /*
-         * @Autowired
-         * DataSource dataSource;
-         * 
-         * @Override
-         * protected void configure(AuthenticationManagerBuilder auth) throws Exception
-         * {
-         * auth.jdbcAuthentication()
-         * .dataSource(dataSource)
-         * .passwordEncoder(passwordEncoder());
-         * auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser(
-         * "spring")
-         * .password(passwordEncoder().encode("thisisfortestsonly"))
-         * .roles("USER");
-         */
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 
-    /*
-     * @Override
-     * protected void configure(HttpSecurity http) throws Exception {
-     * http.authorizeRequests()
-     * .antMatchers("/posts").hasRole("USER")
-     * .antMatchers("/users").permitAll()
-     * .and().formLogin();
-     * 
-     * 
-     * // enable the line below to disable spring security
-     * // http.ignoring().antMatchers("/**");
-     * 
-     * http.csrf().disable();
-     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().authorizeRequests()
@@ -73,6 +40,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 // this disables session creation on Spring Security
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.csrf().disable();
     }
 
     @Bean
