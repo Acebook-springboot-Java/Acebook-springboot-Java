27e543a33acc2c92c8d7b961741a72509c9d8de5
Matt Clements <matthewlclements@gmail.com>
2022-06-23 15:45:57 +0100

    Changes to security config

--- src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
@@ -1,22 +1,38 @@
 package com.makersacademy.acebook;
 
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.makersacademy.acebook.security.*;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private UserDetailsService userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    public SecurityConfiguration(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+      this.userDetailsService = userDetailsService;
+      this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        /*
     @Autowired
     DataSource dataSource;
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
@@ -25,23 +41,47 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("spring")
                 .password(passwordEncoder().encode("thisisfortestsonly"))
                 .roles("USER");
+      */
     }
 
+    /*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/posts").hasRole("USER")
                 .antMatchers("/users").permitAll()
                 .and().formLogin();
+                
 
         // enable the line below to disable spring security
         // http.ignoring().antMatchers("/**");
 
-        http.csrf().disable();
+        http.csrf().disable();*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().authorizeRequests()
+                .antMatchers("/users/new").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+      final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+      CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+      source.registerCorsConfiguration("/**", corsConfiguration);
+
+      return source;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+  }
 }
