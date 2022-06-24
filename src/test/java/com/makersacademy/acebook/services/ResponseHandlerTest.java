47a30ec5d5630f903569b3486f6244eaeed9d429
winstoncwang <congwang@live.co.uk>
2022-06-24 17:22:52 +0100

    added tests

--- src/test/java/com/makersacademy/acebook/services/ResponseHandlerTest.java
+++ src/test/java/com/makersacademy/acebook/services/ResponseHandlerTest.java
@@ -1,37 +1,41 @@
 package com.makersacademy.acebook.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.makersacademy.acebook.model.User;
 
-// public class ResponseHandlerTest {
+@SpringBootTest
+public class ResponseHandlerTest {
+    // private User testUser = new User("testUser1", "testPass1");
+    @Mock
+    User testUser;
 
-// @Test
-// public void shouldHaveASpecificStructure() {
-// User testUser = mock(User.class);
-// ResponseEntity<?> testResponse =
-// ResponseHandler.generateResponse(HttpStatus.OK, true, "this is a test
-// message",
-// testUser);
-// assertThat(testResponse).isNotNull();
+    @Test
+    public void shouldHaveASpecificStructure() {
+        System.out.println("hi");
+        ResponseEntity<?> actualResponse = ResponseHandler.generateResponse(HttpStatus.OK, true,
+                "this is a test message",
+                testUser);
 
-// // Wed Jun 22 13:04:51 BST 2022
-// SimpleDateFormat formatter = new SimpleDateFormat("E MMM dd HH:mm:ss z
-// yyyy");
-// Date date = new Date(System.currentTimeMillis());
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("timestamp", new Date());
+        map.put("status", HttpStatus.OK.value());
+        map.put("isSuccess", true);
+        map.put("message", "this is a test message");
+        map.put("data", testUser);
+        ResponseEntity<?> expectedResponse = new ResponseEntity<Object>(map, HttpStatus.OK);
+        assertThat(actualResponse.getStatusCode().toString()).isEqualTo(expectedResponse.getStatusCode().toString());
+        assertThat(actualResponse.getBody().toString()).isEqualTo(actualResponse.getBody().toString());
+    }
 
-// assertThat(testResponse.getStatusCode().toString()).isEqualTo("200 OK");
-// System.out.println(testResponse.getBody());
-// assertThat(testResponse.getBody().toString()).contains(
-// "message=this is a test message, timestamp=" + formatter.format(date)
-// + ", status=200, isSuccess=true");
-// }
-// }
+}
