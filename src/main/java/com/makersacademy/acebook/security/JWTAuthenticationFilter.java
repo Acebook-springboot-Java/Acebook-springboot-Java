e6ca30b6aaf3dc9dfdfd8127418f1006ebbd6b8b
winstoncwang <congwang@live.co.uk>
2022-06-28 11:21:48 +0100

    cookie samesite added

--- src/main/java/com/makersacademy/acebook/security/JWTAuthenticationFilter.java
+++ src/main/java/com/makersacademy/acebook/security/JWTAuthenticationFilter.java
@@ -1,28 +1,28 @@
 package com.makersacademy.acebook.security;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-
-import javax.servlet.FilterChain;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.makersacademy.acebook.configuration.SecurityConstants;
+import com.makersacademy.acebook.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.makersacademy.acebook.configuration.SecurityConstants;
-import com.makersacademy.acebook.model.User;
+import javax.servlet.FilterChain;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private static final Logger logger = LogManager.getLogger(JWTAuthenticationFilter.class);
@@ -76,14 +76,24 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 //        Map<String, String> token = new HashMap<>();
 //        token.put("token", access_token);
 
-        Cookie cookie = new Cookie("auth",access_token);
-        cookie.setMaxAge(30 * 60); // expires in 30 minutes
-        cookie.setSecure(true);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/login");
-        logger.info("---------ADDING A COOKIE: {}---------", cookie);
+//        Cookie cookie = new Cookie("auth",access_token);
+//        cookie.setMaxAge(30 * 60); // expires in 30 minutes
+//        cookie.setSecure(true);
+//        cookie.setHttpOnly(true);
+//        cookie.setPath("/login");
+
+        ResponseCookie responseCookie = ResponseCookie.from("auth",access_token)
+                .maxAge(30 * 60)
+                .secure(true)
+                .httpOnly(true)
+                .sameSite("Lax")
+                .build();
+
+//        logger.info("---------ADDING A COOKIE: {}---------", cookie);
+        logger.info("---------ADDING A COOKIE: {}---------", responseCookie);
         res.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        res.addCookie(cookie);
+//        res.addCookie(cookie);
+        res.addHeader(HttpHeaders.SET_COOKIE,responseCookie.toString());
         new ObjectMapper().writeValue(res.getOutputStream(), "authentication successful for user "+authenticatedSecurityUser.getUsername());
     }
 
