b9abe1c3eca54d6bca8f300773e841d0ec81a361
Matt Clements <matthewlclements@gmail.com>
2022-06-29 16:41:10 +0100

    Delete done at least!

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -1,14 +1,19 @@
 package com.makersacademy.acebook.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
 
 import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.repository.PostRepository;
@@ -34,4 +39,39 @@ public class PostsController {
         postRepository.save(post);
         return ResponseHandler.generateResponse(HttpStatus.OK, true, "posts created", post);
     }
+
+    @PatchMapping("/posts/{id}")
+    public ResponseEntity<?> update(@RequestBody Post post, @PathVariable("id") Long id) {
+        logger.info("---------PATCH request sent to /posts{id}------");
+        try {
+          String content = post.toString();
+          Post existingPost = postRepository.findById(id).get();
+          Post updatedPost = existingPost.setContent(content);
+          return ResponseHandler.generateResponse(HttpStatus.OK, true, "post edited", updatedPost);}
+        catch (Exception e) {
+          return ResponseHandler.generateResponse(HttpStatus.NOT_FOUND, false, "post not found", post)
+        }
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        logger.info("---------POST request send to /posts---------");
+        postRepository.deleteById(id);
+        return ResponseHandler.generateResponse(HttpStatus.OK, true, "post deleted", post);
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<?> getPost(@PathVariable("id") Long id) {
+        logger.info("---------GET request send to /posts{id}------");
+    }
+}
+
+
+try {
+  Employee employee = employeeRepository.findById(id).get();
+  employee.setFirstName(firstName);
+  return new ResponseEntity<Employee>(employeeRepository.save(employee), HttpStatus.OK);
+} catch (Exception e) {
+  return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+}
 }
--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.makersacademy.acebook.model.Post;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
+  Post findById(int id);
 
 }
