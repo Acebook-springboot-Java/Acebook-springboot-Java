c47de9c3e9e5c67df54434cc80886c2df497cb9d
Matt Clements <91874346+mlclem@users.noreply.github.com>
2022-07-01 10:25:06 +0100

    Revert "New features"

--- src/main/resources/db/migration/V4__create_followers_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS followers;
-
-CREATE TABLE followers (
-  id bigserial PRIMARY KEY,
-  user_id bigint NOT NULL,
-  follower_id bigint NOT NULL,
-  pending boolean NOT NULL,
-  accepted boolean NOT NUll
-);
